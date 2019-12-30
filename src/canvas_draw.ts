5b7971f8e824947be7ea226b0a5dd5abda56d01a
Daniel Jawna <djawna@gmail.com>
2019-12-30 18:30:24 +0100

    add new class: canvas_draw this class now encapsulates the drawing ops

--- src/canvas_draw.ts
+++ src/canvas_draw.ts
@@ -1,14 +1,53 @@
 
-export const clearCanvas =function (canvasCtxt: any,topX: number,topY: number,Width: number,Height: number): void {
-    canvasCtxt.save();
+class canvas_draw {
+    readonly canvasCtxt : CanvasRenderingContext2D;
 
-    canvasCtxt.setTransform(1,0,0,1,0,0);
+    public constructor(document : Document, canvasID: string,width: number, height: number){
+        let lookedUpElement  = document.getElementById(canvasID);
+        if(lookedUpElement == null) throw `the following canvasID: ${canvasID} does not exist in the html document!`;
+        let canvasElement : HTMLCanvasElement = <HTMLCanvasElement> lookedUpElement;
+        canvasElement.width = width;
+        canvasElement.height = height;
+        const retval = canvasElement.getContext( "2d");
+        if(retval === null) throw "Could not create the 2d rendering context!";
+        this.canvasCtxt = retval;
+    }
 
-    canvasCtxt.clearRect(topX,topY,Width,Height);
+    public clearCanvas (topX: number,topY: number,Width: number,Height: number): void {
+        this.canvasCtxt.save();
+    
+        this.canvasCtxt.setTransform(1,0,0,1,0,0);
+    
+        this.canvasCtxt.clearRect(topX,topY,Width,Height);
+    
+        this.canvasCtxt.restore();
+    }
 
-    canvasCtxt.restore();
-};
+    public fillCanvas(x: number, y: number, width: number, height: number, color: string, opacity: number): void {
+        this.canvasCtxt.globalAlpha = opacity;
+        this.canvasCtxt.fillStyle = color;
+        this.canvasCtxt.fillRect(x, y, width, height);
+        this.canvasCtxt.globalAlpha = 1.0;
+    }
+    
+    public drawCellTexture(texture: Texture,x: number,y: number,width: number,height: number,opacity:number): void{
+        this.canvasCtxt.globalAlpha = opacity;
+        this.canvasCtxt.drawImage(texture.img,
+                             texture.sx,
+                             texture.sy,
+                             texture.swidth,
+                             texture.sheight,
+                             x,
+                             y,
+                             width,
+                             height);
+        this.canvasCtxt.globalAlpha = 1.0;
+        this.canvasCtxt.globalAlpha = 1.0;
+    }
+}
 
+
+// no longer needed, the constructor will do that!
 export const setupCanvas = function (document : any, canvasID: string,width: number, height: number): any {
     let canvasElement = document.getElementById(canvasID);
     canvasElement.width = width;
@@ -54,3 +93,15 @@ export class Texture{
     }
 
 }
+
+
+
+export const clearCanvas =function (canvasCtxt: any,topX: number,topY: number,Width: number,Height: number): void {
+    canvasCtxt.save();
+
+    canvasCtxt.setTransform(1,0,0,1,0,0);
+
+    canvasCtxt.clearRect(topX,topY,Width,Height);
+
+    canvasCtxt.restore();
+};
