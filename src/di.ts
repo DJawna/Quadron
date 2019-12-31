95826dcdcbd3b02a0652307556e70a037dd94587
daniel <djawna@gmail.com>
2019-12-31 15:41:15 +0100

    add sprite list

--- src/di.ts
+++ src/di.ts
@@ -1,9 +1,10 @@
 import {IRenderer} from "./draw_contracts";
 import {canvas_draw} from "./canvas_draw";
+import {pixi_renderer} from "./pixi_renderer";
 /**
  * This is the DI Registry
  */
 
 export const getRenderer = function(window: Window, width: number,height: number): IRenderer {
-    return new canvas_draw(window,width,height)
+    return /* new pixi_renderer(window,width,height); */ new canvas_draw(window,width,height)
 }
--- src/pixi_renderer.ts
+++ src/pixi_renderer.ts
@@ -6,31 +6,41 @@ export class pixi_renderer implements IRenderer{
     readonly app: pix.Application; 
     readonly tLoader: pix.Loader;
     readonly loadedTextures: string[]=[];
+    readonly cellSprites : pix.Sprite[] = [];
+    firstCanvasClearing: boolean;
 
     constructor(window : Window, width: number, height: number) {
+        this.firstCanvasClearing = false;
         let lookedUpElement  = window.document.getElementById("playArea");
         this.tLoader = new pix.Loader();
         if(lookedUpElement == null) throw "playArea Element does not exist!";
 
         this.app = new pix.Application( {width,height});
         lookedUpElement.appendChild(this.app.view);
+
+
+
     }
 
     public clearCanvas(topX: number, topY: number, Width: number, Height: number): void {
-        const rect = new pix.Graphics();
-        rect.beginFill(0x000000);
-        rect.drawRect(topX,topY,Width,Height);
-        rect.endFill();
-        rect.x=0;
-        rect.y=0;
-        this.app.stage.addChild(rect);
+        if(this.firstCanvasClearing){
+            const rect = new pix.Graphics();
+            rect.beginFill(0x000000);
+            rect.drawRect(topX,topY,Width,Height);
+            rect.endFill();
+            rect.x=0;
+            rect.y=0;
+            this.app.stage.addChild(rect);
+            this.firstCanvasClearing= false;
+        }
+        this.cellSprites.forEach(i => i.alpha = 0);
+
     }
 
     public drawCellTexture(texture: Texture, x: number, y: number, width: number, height: number, opacity: number): void {
         const drawCommand = () => {
-            const sprite = new pix.Sprite(
-                this.tLoader.resources[texture.img].texture
-            );
+            const sprite = new pix.Sprite();
+            sprite.texture = this.tLoader.resources[texture.img].texture;
             sprite.x = x;
             sprite.y = y;
             sprite.width = width;
@@ -40,6 +50,7 @@ export class pixi_renderer implements IRenderer{
         };
         
         if (!this.loadedTextures.includes(texture.img)){
+            this.loadedTextures.push(texture.img);
             this.tLoader.add(texture.img).load(drawCommand);
         }else{
             drawCommand();
@@ -48,6 +59,8 @@ export class pixi_renderer implements IRenderer{
 
     public flushDrawBuffers(): void {
         this.app.render();
+        
+        
     }
 
 }
