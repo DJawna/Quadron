d72a008575db81bac7d6516b8fcf2628691b9f2a
daniel <djawna@gmail.com>
2019-12-31 11:10:40 +0100

    finish the constructor of pixi renderer

--- src/pixi_renderer.ts
+++ src/pixi_renderer.ts
@@ -1,3 +1,16 @@
+import * as pix from "pixi.js";
 
+export class pixi_renderer{
+    readonly app: pix.Application; 
 
-export class pixi_renderer{}
+    constructor(document : Document, width: number, height: number) {
+        let lookedUpElement  = document.getElementById("playArea");
+        if(lookedUpElement == null) throw "playArea Element does not exist!";
+
+        this.app = new pix.Application( {width,height});
+        lookedUpElement.appendChild(this.app.view);
+    }
+
+    
+
+}
