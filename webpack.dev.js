2d1e93069bec62be3688d8f0a53f41d399f75443
daniel <djawna@gmail.com>
2020-01-15 20:03:59 +0100

    change the host version

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
     mode: "development",
     devtool: "inline-source-map",
     devServer: {
-        contentBase: './dist'
-    }
+        contentBase: './dist',
+        host: '0.0.0.0'
+    },
 });
